458688d5f729b414735ae9cc05d9928e33148b66
bohdankorchak <bohdankorchak@gmail.com>
2024-11-17 22:27:05 +0200

    Add route builder

--- app/services/route_builder.py
+++ app/services/route_builder.py
@@ -1,9 +1,65 @@
-import requests
+from backend.app.models.route_model import save_route_to_db, save_segment_to_db
+from backend.app.services.utils import decode_polyline
+from backend.app.services.traffic_data_service import GoogleAPIConnector
+
 
 class RouteBuilder:
-    def __init__(self, traffic_data_service):
-        self.traffic_data_service = traffic_data_service
 
-    def build_route(self, start, end, waypoints=None):
-        # Логіка для побудови маршруту
-        pass
+    async def build_routes(self, origin: tuple, destination: tuple, db_session):
+        origin_str = f"{origin[0]},{origin[1]}"
+        destination_str = f"{destination[0]},{destination[1]}"
+
+        routes = await GoogleAPIConnector.get_routes(origin_str, destination_str)
+        processed_routes = []
+        for route in routes:
+            route_segments = []
+            for leg in route["legs"]:
+                await save_route_to_db(leg, db_session)
+                route_segments = await self.prepare_segments(leg["steps"],  db_session)
+
+            processed_routes.append({
+                "summary": route["summary"],
+                "distance": route["legs"][0]["distance"]["text"],
+                "duration": route["legs"][0]["duration_in_traffic"]["text"],
+                "segments": route_segments
+            })
+
+        return processed_routes
+
+    async def prepare_segments(self, steps,  db_session):
+        route_segments = []
+        for step in steps:
+            start_location = f'{step["start_location"]["lat"]},{step["start_location"]["lng"]}'
+            end_location = f'{step["end_location"]["lat"]},{step["end_location"]["lng"]}'
+
+            traffic_data = await GoogleAPIConnector.get_segment_traffic(
+                start_location, end_location
+            )
+
+            duration_in_traffic = traffic_data["duration_in_traffic"]["value"]
+            normal_duration = traffic_data["duration"]["value"]
+            if normal_duration > 0:
+                traffic_ratio = duration_in_traffic / normal_duration
+                if traffic_ratio > 1.4:
+                    color = "red"
+                elif 1.25 <= traffic_ratio <= 1.4:
+                    color = "yellow"
+                else:
+                    color = "green"
+            else:
+                color = "blue"
+
+            polyline = decode_polyline(step["polyline"]["points"])
+
+            segment = {
+                "start_location": step["start_location"],
+                "end_location": step["end_location"],
+                "duration_in_traffic": duration_in_traffic,
+                "normal_duration": normal_duration,
+                "polyline": step["polyline"]["points"]
+            }
+            await save_segment_to_db(segment, db_session)
+            route_segments.append({"polyline": polyline, "color": color})
+
+        return route_segments
+
---
+++ app/services/utils.py
@@ -0,0 +1,27 @@
+def decode_polyline(encoded):
+    points = []
+    index = lat = lng = 0
+
+    while index < len(encoded):
+        result = shift = 0
+        while True:
+            b = ord(encoded[index]) - 63
+            index += 1
+            result |= (b & 0x1f) << shift
+            shift += 5
+            if b < 0x20:
+                break
+        lat += ~(result >> 1) if (result & 1) else (result >> 1)
+
+        result = shift = 0
+        while True:
+            b = ord(encoded[index]) - 63
+            index += 1
+            result |= (b & 0x1f) << shift
+            shift += 5
+            if b < 0x20:
+                break
+        lng += ~(result >> 1) if (result & 1) else (result >> 1)
+
+        points.append([lat / 1e5, lng / 1e5])
+    return points
