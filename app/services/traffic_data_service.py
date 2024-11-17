5286ecc99c45c8235528ea6ae7b6384343bd1b0a
bohdankorchak <bohdankorchak@gmail.com>
2024-11-17 22:26:18 +0200

    Add google api connector

--- app/services/traffic_data_service.py
+++ app/services/traffic_data_service.py
@@ -1,9 +1,44 @@
 import requests
+from backend.app.config import Config
 
-class TrafficDataService:
-    def __init__(self, api_key):
-        self.api_key = api_key
 
-    def get_traffic_data(self, start, end):
-        # Логіка для отримання даних про трафік з API
-        pass
+class GoogleAPIConnector:
+
+    BASE_URL = Config.GOOGLE_BASE_URL
+
+    @staticmethod
+    async def get_routes(origin: str, destination: str, alternatives: bool = True, departure_time: str = "now"):
+        params = {
+            "origin": origin,
+            "destination": destination,
+            "alternatives": "true" if alternatives else "false",
+            "key": Config.GOOGLE_MAPS_API_KEY,
+            "departure_time": departure_time,
+        }
+
+        response = requests.get(GoogleAPIConnector.BASE_URL, params=params)
+        response.raise_for_status()
+        data = response.json()
+
+        if data["status"] != "OK":
+            raise ValueError(f"Error from Google Maps API: {data['status']}")
+
+        return data["routes"]
+
+    @staticmethod
+    async def get_segment_traffic(start_location: str, end_location: str, departure_time: str = "now"):
+        params = {
+            "origin": start_location,
+            "destination": end_location,
+            "key": Config.GOOGLE_MAPS_API_KEY,
+            "departure_time": departure_time,
+        }
+
+        response = requests.get(GoogleAPIConnector.BASE_URL, params=params)
+        response.raise_for_status()
+        data = response.json()
+
+        if data["status"] != "OK":
+            raise ValueError(f"Error from Google Maps API: {data['status']}")
+
+        return data["routes"][0]["legs"][0]
