c75f26ccf023e4b37ce4aed2bbacc98eac3d4454
bohdankorchak <bohdankorchak@gmail.com>
2024-11-17 22:27:26 +0200

    Add route controller

--- app/controllers/route_controller.py
+++ app/controllers/route_controller.py
@@ -1,7 +1,23 @@
-from fastapi import APIRouter
+from typing import Tuple
+
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from backend.app.db import get_async_session
+from backend.app.services.route_builder import RouteBuilder
 
 router = APIRouter()
 
-@router.post("/route")
-async def create_route():
-    return {"message": "Route created"}
+
+@router.post("/routes")
+async def get_routes(
+    origin: Tuple[float, float],
+    destination: Tuple[float, float],
+    db_session: AsyncSession = Depends(get_async_session)
+):
+    try:
+        route_builder = RouteBuilder()
+        routes = await route_builder.build_routes(origin, destination, db_session)
+        return {"status": "success", "routes": routes}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
